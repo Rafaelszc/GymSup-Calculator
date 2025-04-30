fd57d9e7a37cf19f5c889998174b8448c3aa159a
Rafaelszc <rafaelbjj84@gmail.com>
2025-04-30 11:34:11 -0300

    add: tests added

---
+++ src/test/java/com/spring/study/GymSup/GymSupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.study.GymSup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymSupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
