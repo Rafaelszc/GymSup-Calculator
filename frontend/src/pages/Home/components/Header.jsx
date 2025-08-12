70f01acce3c43fe5a1b6da567a0a36c7f24afc5e
Rafaelszc <rafaelbjj84@gmail.com>
2025-08-12 20:50:26 -0300

    style: navbar removed

--- frontend/src/pages/Home/components/Header.jsx
+++ frontend/src/pages/Home/components/Header.jsx
@@ -1,26 +1,9 @@
-import { Link } from "react-router-dom"
-
 export const Header = () => {
-    const naviagation = [
-        {name: "Home", link: "#home"},
-        {name: "History", link: "#history"}
-    ]
-
     return (
-        <header className="h-20 shadow-sm flex items-center justify-between pr-14 pl-14">
+        <header className="h-20 shadow-sm flex items-center justify-between pr-14 pl-14 z-0">
             <div>
-                <span className="text-2xl font-bold"><Link to={'/'}>GymSup 🍎</Link></span>
+                <span className="text-2xl font-bold"><a href="">GymSup 🍎</a></span>
             </div>
-            <nav>
-                <ul className="flex gap-8 list-none">
-                    {naviagation.map((nav, i) => (
-                        <li key={i} className="border-b-2 border-white transition transform duration-175 ease-linear hover:border-blue-500 ">
-                            <a href={nav.link}>{nav.name}</a>
-                        </li>
-                    ))}
-                </ul>
-
-            </nav>
         </header>
     )
 }
