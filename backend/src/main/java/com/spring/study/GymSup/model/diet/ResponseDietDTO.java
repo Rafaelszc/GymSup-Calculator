64640cd3088ce5882916677618de7d9e34b7ba0b
Rafaelszc <rafaelbjj84@gmail.com>
2025-09-01 23:25:26 -0300

    feat: include 'total' to ResponseDietDTO & change 'dietType' parameter of RequestPersonDTO to 'diet'

--- backend/src/main/java/com/spring/study/GymSup/model/diet/ResponseDietDTO.java
+++ backend/src/main/java/com/spring/study/GymSup/model/diet/ResponseDietDTO.java
@@ -1,12 +1,12 @@
 package com.spring.study.GymSup.model.diet;
 
-public record ResponseDietDTO(Long id, String timestamp, double calories,
-                              double proteins, double carbo, double fat) {
+public record ResponseDietDTO(String timestamp, double calories, double proteins,
+                              double carbo, double fat, int total) {
 
     public ResponseDietDTO(Diet diet) {
         this(
-            diet.getId(), diet.getTimestamp(), diet.getCalories(),
-            diet.getProteins(), diet.getCarbo(), diet.getFat()
+                diet.getTimestamp(), diet.getCalories(), diet.getProteins(),
+                diet.getCarbo(), diet.getFat(), diet.getTotal()
         );
     }
 }
--- backend/src/main/java/com/spring/study/GymSup/model/person/RequestPersonDTO.java
+++ backend/src/main/java/com/spring/study/GymSup/model/person/RequestPersonDTO.java
@@ -5,14 +5,14 @@ import com.spring.study.GymSup.enums.PersonRoutine;
 import com.spring.study.GymSup.enums.Sex;
 
 public record RequestPersonDTO(int height, double weight, int age,
-                               Sex sex, DietType dietType, PersonRoutine routine) {
+                               Sex sex, DietType diet, PersonRoutine routine) {
 
-    public RequestPersonDTO(int height, double weight, int age, Sex sex, DietType dietType, PersonRoutine routine) {
+    public RequestPersonDTO(int height, double weight, int age, Sex sex, DietType diet, PersonRoutine routine) {
         this.height = height;
         this.weight = weight;
         this.age = age;
         this.sex = sex;
-        this.dietType = dietType;
+        this.diet = diet;
         this.routine = routine;
     }
 }
