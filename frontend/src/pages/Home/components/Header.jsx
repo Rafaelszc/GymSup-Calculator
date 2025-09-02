dbc98157c11be722724f813c9b4754d457b63933
Rafaelszc <rafaelbjj84@gmail.com>
2025-09-02 17:46:19 -0300

    chore: use Link from react router to link home page

--- frontend/src/pages/Home/components/Header.jsx
+++ frontend/src/pages/Home/components/Header.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom"
+
 export const Header = () => {
     return (
         <header className="h-20 shadow-sm flex items-center justify-between pr-14 pl-14 z-0">
             <div>
-                <span className="text-2xl font-bold"><a href="">GymSup 🍎</a></span>
+                <span className="text-2xl font-bold"><Link to="/">GymSup 🍎</Link></span>
             </div>
         </header>
     )
